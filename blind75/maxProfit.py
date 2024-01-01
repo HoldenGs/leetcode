c6976718b53df1cd9d7ce05e08cae88b9609a45e
Holden Grissett <holdeng96@gmail.com>
2024-01-01 10:30:29 -0800

    formatting
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- blind75/maxProfit.py
+++ blind75/maxProfit.py
@@ -1,11 +1,11 @@
 def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-        min_price = prices[0]
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            profit = price - min_price
-            if profit > max_profit:
-                max_profit = profit
+    max_profit = 0
+    min_price = prices[0]
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        profit = price - min_price
+        if profit > max_profit:
+            max_profit = profit
 
-        return max_profit
+    return max_profit
--- blind75/validParentheses.py
+++ blind75/validParentheses.py
@@ -1,14 +1,14 @@
 def isValid(self, s: str) -> bool:
-        stack = []
-        matches = {')': '(', ']': '[', '}': '{'}
-        for c in s:
-            if c in "({[":
-                stack.append(c)
-            elif len(stack) == 0 or stack[-1] != matches[c]:
-                return False
-            else:
-                stack.pop()
-
-        if len(stack) > 0:
+    stack = []
+    matches = {')': '(', ']': '[', '}': '{'}
+    for c in s:
+        if c in "({[":
+            stack.append(c)
+        elif len(stack) == 0 or stack[-1] != matches[c]:
             return False
-        return True
+        else:
+            stack.pop()
+
+    if len(stack) > 0:
+        return False
+    return True
