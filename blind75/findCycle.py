5f94e2951a753bc9306b4d3307010da2a3006cfb
Holden Grissett <holdeng96@gmail.com>
2024-01-01 19:37:10 -0800

    find linked list cycle
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

---
+++ blind75/findCycle.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def hasCycle(head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast and slow:
+            fast = fast.next
+            if fast == slow and fast != None:
+                return True
+            
+            slow = slow.next
+            if fast != None:
+                fast = fast.next
+        
+        return False
